6ba278e39d01ca9e55ab0948ab653d963b6a5868
glennwanjiru <glennnjoroge@gmail.com>
2023-09-16 21:23:00 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Motive Tea',
+        title: 'Daily Random Quotes',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
